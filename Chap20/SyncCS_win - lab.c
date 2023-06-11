ab966d4879f1e3dd4cc00e05b9219edadb7e8568
EunJu JO <dmswnehf@naver.com>
2023-06-11 16:00:37 +0900

    Add files via upload

---
+++ Chap20/SyncCS_win - lab.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <windows.h>
+#include <process.h>
+#define NUM_THREAD	50
+
+unsigned WINAPI threadInc(void * arg);
+unsigned WINAPI threadDes(void * arg);
+
+long long num=0;
+CRITICAL_SECTION cs;
+
+int main(int argc, char *argv[]) 
+{
+	HANDLE tHandles[NUM_THREAD];
+	int i;
+
+
+	InitializeCriticalSection(&cs);
+	// 스레드 생성...
+	for(i=0; i<NUM_THREAD; i++)
+	{
+		if (i % 2)
+			tHandles[i] = (HANDLE)_beginthreadex(NULL, 0, threadInc, NULL, 0, NULL);
+		else
+			tHandles[i] = (HANDLE)_beginthreadex(NULL, 0, threadDes, NULL, 0, NULL);
+	}
+	
+	WaitForMultipleObjects(NUM_THREAD, tHandles, TRUE, INFINITE);
+	DeleteCriticalSection(&cs);
+	printf("result: %lld \n", num);
+	return 0;
+}
+
+unsigned WINAPI threadInc(void * arg)
+{
+	int i;
+
+	// num 접근(증가)...
+	EnterCriticalSection(&cs);
+	for (i = 0; i < 50000000; i++)
+		num += 1;
+	LeaveCriticalSection(&cs);
+	return 0;
+}
+
+unsigned WINAPI threadDes(void * arg)
+{
+	int i;
+
+	// num 접근(감소)...
+	EnterCriticalSection(&cs);
+	for (i = 0; i < 50000000; i++)
+		num -= 1;
+	LeaveCriticalSection(&cs);
+	return 0;
+}
